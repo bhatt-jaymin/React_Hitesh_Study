77e8fc247fd63a75e444ff812a1057926e7a3cba
jaymin1088 <jaymin_10_88@yahoo.co.in>
2024-01-17 00:59:24 +0530

    vital react
    explain methods in vital js

--- 01_react/example1/01_ViteReact/src/App.jsx
+++ 01_react/example1/01_ViteReact/src/App.jsx
@@ -1,11 +1,13 @@
 import Reactvite1 from "./Reactvite";
 
 function App() {
+  const userName='React Dev' //evaluate expression of variable
+
   return (
     //fragment start
     <>
       <Reactvite1 />
-      <h3><p>Simple Example of the React Vite</p></h3>
+      <h3><p>Simple Example of the {userName} in React Vite</p></h3>
     </>
     //fragment End
   );
--- 01_react/example1/01_ViteReact/src/main.jsx
+++ 01_react/example1/01_ViteReact/src/main.jsx
@@ -3,9 +3,34 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+function MyApp() {
+    
+    return(
+        <>
+        <h1>Custom App !</h1>
+        </>
+    )
+}
+
+/* const MyHtml=(
+    <a href='https://google.com'>visit google </a>
+) */
+
+// First create react  element using createelement 
+
+const firstCreateReactElement=React.createElement(                              
+                              'a',
+                              {href:"https://google.com", target:'_blank'},                              
+                              'click me to visit Google'  
+) 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
  
-    <App />
+   // firstCreateReactElement
+   <App/>
+   //<MyApp/>
+   //<firstCreateReactElement/>
+   
   
 )
 
